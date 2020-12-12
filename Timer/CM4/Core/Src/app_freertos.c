b0ac9f97b22d1f9d39c16a56bd32ad02467cb192
@zhanqinghui <1442190496@qq.com>
2020-12-12 15:04:25 +0800

    软件定时器实验

---
+++ Timer/CM4/Core/Src/app_freertos.c
@@ -0,0 +1,185 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * File Name          : app_freertos.c
+  * Description        : Code for freertos applications
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "./led/bsp_led.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+static uint32_t TmrCb_Count1 = 0; /* 记录软件定时器1回调函数执行次数 */
+static uint32_t TmrCb_Count2 = 0; /* 记录软件定时器2回调函数执行次数 */
+
+/* USER CODE END Variables */
+/* Definitions for defaultTask */
+osThreadId_t defaultTaskHandle;
+const osThreadAttr_t defaultTask_attributes = {
+  .name = "defaultTask",
+  .priority = (osPriority_t) osPriorityNormal,
+  .stack_size = 128 * 4
+};
+/* Definitions for Timer01 */
+osTimerId_t Timer01Handle;
+const osTimerAttr_t Timer01_attributes = {
+  .name = "Timer01"
+};
+/* Definitions for Timer02 */
+osTimerId_t Timer02Handle;
+const osTimerAttr_t Timer02_attributes = {
+  .name = "Timer02"
+};
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+
+/* USER CODE END FunctionPrototypes */
+
+void StartDefaultTask(void *argument);
+void Swtmr1_Callback(void *argument);
+void Swtmr2_Callback(void *argument);
+
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* Create the timer(s) */
+  /* creation of Timer01 */
+  Timer01Handle = osTimerNew(Swtmr1_Callback, osTimerPeriodic, NULL, &Timer01_attributes);
+
+  /* creation of Timer02 */
+  Timer02Handle = osTimerNew(Swtmr2_Callback, osTimerOnce, NULL, &Timer02_attributes);
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  osTimerStart (Timer01Handle, 1000);
+  osTimerStart (Timer02Handle, 5000);
+
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of defaultTask */
+  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+}
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void *argument)
+{
+  /* USER CODE BEGIN StartDefaultTask */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartDefaultTask */
+}
+
+/* Swtmr1_Callback function */
+void Swtmr1_Callback(void *argument)
+{
+	  uint32_t tick_num1;
+
+	  TmrCb_Count1++;						/* 每回调一次加一 */
+
+	  tick_num1 = osKernelGetTickCount();	/* 获取滴答定时器的计数值 */
+
+	  LED1_TOGGLE;
+
+	  printf("Swtmr1_Callback函数执行 %lu 次\n", TmrCb_Count1);
+	  printf("滴答定时器数值=%lu\n", tick_num1);
+}
+
+/* Swtmr2_Callback function */
+void Swtmr2_Callback(void *argument)
+{
+	/* USER CODE BEGIN Swtmr2_Callback */
+	uint32_t tick_num2;
+
+	TmrCb_Count2++;						/* 每回调一次加一 */
+
+	tick_num2 = osKernelGetTickCount();	/* 获取滴答定时器的计数值 */
+
+	printf("Swtmr2_Callback函数执行 %lu 次\n", TmrCb_Count2);
+	printf("滴答定时器数值=%lu\n", tick_num2);
+	/* USER CODE END Swtmr2_Callback */
+}
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+
+/* USER CODE END Application */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
